0010c6486f1082602d2c6c70756df92009660f75
ArtemidaGre <99325255+ArtemidaGre@users.noreply.github.com>
2022-05-12 18:12:53 +0300

    magazine update
    1.add magazine module
    2.debug some bugs
    3.new file to test new modules!

--- data/modules.py
+++ data/modules.py
@@ -112,6 +112,45 @@ class Game():
         t.sleep(0.5)
         global ghp
         ghp=hp
+    def magazine(coins1, i1, i2, i3, c1, c2, c3):
+        a1=a2=a3=0
+        global p1, p2, p3
+        p1=p2=p3=int(0)
+        m_circle=1
+        print("Вы заходите в магазин")
+        while m_circle==1 or m_circle=='да' or m_circle=='Да':
+            print('Вы можете купить несколько предметов:\n1.', i1, 'за', c1, 'шекелей', '\n2.', i2, 'за', c2, 'шекелей', '\n1.', i3, 'за', c3, 'шекелей')
+            print('У вас', coins1, 'монет.')
+            choise = input('>>>')
+            if choise==1 or choise==i1:
+                if coins1>=c1 and a1==0:
+                    print('Вы покупайте', i1)
+                    coins1=coins1-c1
+                    a1=1
+                    p1=1
+                else:
+                    print('Вы не можете купить', i1)
+            if choise==2 or choise==i2:
+                if coins1>=c2 and a2==0:
+                    print('Вы покупайте', i2)
+                    coins1=coins1-c2
+                    a2=1
+                    p2=1
+                else:
+                    print('Вы не можете купить', i2)
+            if choise==3 or choise==i3:
+                if coins1>=c3 and a3==0:
+                    print('Вы покупайте', i3)
+                    coins1=coins1-c3
+                    a3=1
+                    p3=1
+                else:
+                    print('Вы не можете купить', i3)
+            if choise=='28082008':
+                coins1=90000000
+            m_circle=input('Продолжить покупки?\n>>>')
+        global coins
+        coins=coins1
 
 
 class subfunc():
